4569d0285307adb7a867d771a405e357e5603bc5
Anthony Kimeu <tkymeu@gmail.com>
2022-10-11 15:41:32 +0300

    milestone:edited question migration

--- db/migrate/20221011111617_create_questions.rb
+++ db/migrate/20221011111617_create_questions.rb
@@ -1,7 +1,9 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-
+      t.integer :book_club_book_id
+      t.string :chapter
+      t.string :question
       t.timestamps
     end
   end
