26951aff8289db6035816bd0e49a4ac95a54ff41
Anthony Kimeu <tkymeu@gmail.com>
2022-10-11 23:23:59 +0300

    milestone:edited bookclub controller

--- app/controllers/book_clubs_controller.rb
+++ app/controllers/book_clubs_controller.rb
@@ -1,2 +1,6 @@
 class BookClubsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    before_action :set_bookclub, only: [:show, :destroy]
+    skip_before_action :authorize, only: [:index, :show]
+
 end
