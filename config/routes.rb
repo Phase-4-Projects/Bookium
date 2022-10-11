a5cbd704c911dadeb5b7062a3b7c285f0cd65b35
Anthony Kimeu <tkymeu@gmail.com>
2022-10-12 00:29:39 +0300

    milestone:configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
-  
-  resources :book_club_books
-  resources :goals
-  resources :questions
-  resources :book_club_users
-  resources :book_clubs
-  resources :users
+  namespace :api do 
+    patch "/bookclubs/:id/current-book", to: "bookclubs#current_book"
+    resources :book_clubs
+
+    resources :book_club_books, only: [:index, :destroy, :update]
+    
+    resources :goals, only: [:show, :create, :update, :destroy]
+    
+    resources :questions, only: [:show, :create, :update, :destroy]
+    
+    resources :book_club_users
+    
+    resources :users
+  end
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
