d3de090400882a900b456fe78820609d6be3b28a
Anthony Kimeu <tkymeu@gmail.com>
2022-10-11 23:31:45 +0300

    milestone:updated bookclub controller

--- app/controllers/book_clubs_controller.rb
+++ app/controllers/book_clubs_controller.rb
@@ -17,4 +17,11 @@ class BookClubsController < ApplicationController
             new_admin_bookclub_user = bookclub.bookclub_users.find_by(user_id: params[:admin_id])
             new_admin_bookclub_user.update(isAdmin: true)
         end
+
+         # delete users if needed
+         if !params[:delete_users].empty?
+            users = params[:delete_users].each do |user_id|
+                bookclub_user = bookclub.bookclub_users.find_by(user_id: user_id)
+                bookclub_user.destroy
+            end
 end
