a159a74aabe7418c1c26c4ef387182a16753db92
Anthony Kimeu <tkymeu@gmail.com>
2022-10-11 23:47:42 +0300

    milestone:edited application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,11 @@ class ApplicationController < ActionController::API
 
   before_action :authorize
 
+  private
+    def authorize
+    @current_user = User.find_by_id(session[:user_id])
+    render json: { errors: ["Not Authorized"] }, status: :unauthorized unless @current_user
+   end
+
 
 end
