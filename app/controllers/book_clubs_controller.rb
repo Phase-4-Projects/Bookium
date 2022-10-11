26f14442452027f2d5dddff9c08bedb298d5df56
Anthony Kimeu <tkymeu@gmail.com>
2022-10-11 23:27:29 +0300

    milestone:updated bookclub controller

--- app/controllers/book_clubs_controller.rb
+++ app/controllers/book_clubs_controller.rb
@@ -3,4 +3,8 @@ class BookClubsController < ApplicationController
     before_action :set_bookclub, only: [:show, :destroy]
     skip_before_action :authorize, only: [:index, :show]
 
+
+    def update
+        bookclub = Bookclub.find(params[:id])
+        bookclub.update(bookclub_params)
 end
