91dc5227e6ad6899f92777ed2ab84dc827faa0e3
Anthony Kimeu <tkymeu@gmail.com>
2022-10-12 10:03:37 +0300

    milestone:made changes in bookclub

--- app/models/book_club.rb
+++ app/models/book_club.rb
@@ -1,3 +1,7 @@
 class BookClub < ApplicationRecord
-    belongs_to :book
+    validates :name, {presence: true }
+    has_many :bookclub_users, dependent: :destroy
+    has_many :users, through: :bookclub_users
+    has_many :bookclub_books, dependent: :destroy
+    has_many :books, through: :bookclub_books
 end
