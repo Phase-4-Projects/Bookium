a3f6f0e97b51e0cbda9c51b9300248e795bafad0
Anthony Kimeu <tkymeu@gmail.com>
2022-10-12 10:17:53 +0300

    milestone:added validation in goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,9 @@
 class Goal < ApplicationRecord
-    belongs_to :book_club_book
+  belongs_to :book_club_book
+
+  validates :bookclub_book_id, {presence: true }
+  validates :deadline, {presence: true }
+  validates :pages, {presence: true }
+  validates :pages, numericality: true
+  validates :pages, length: { is: 2 }
 end
