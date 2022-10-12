417fbac0b94ca9133700a57bfc5a5e81e45d14a0
Anthony Kimeu <tkymeu@gmail.com>
2022-10-12 10:09:44 +0300

    milestone:added validation

--- app/models/book_club_book.rb
+++ app/models/book_club_book.rb
@@ -3,4 +3,8 @@ class BookClubBook < ApplicationRecord
   belongs_to :book
   has_many :goals, dependent: :destroy
   has_many :questions, dependent: :destroy
+
+  validates :book_id, {presence: true }
+  validates :bookclub_id, {presence: true }
+  validates :status, inclusion: { in: ['Not Started', 'In Progress', 'Finished']}
 end
