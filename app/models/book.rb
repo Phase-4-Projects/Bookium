af0e6ed120a795930741f4586932227fce33fc01
Anthony Kimeu <tkymeu@gmail.com>
2022-10-12 10:01:17 +0300

    milestone:added book relationship

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,7 @@ class Book < ApplicationRecord
     validates :author, {presence: true }
     validates :imageURL, {presence: true }
     validates :description, {presence: true }
+
+    has_many :bookclub_books, dependent: :destroy
+    has_many :bookclubs, through: :bookclub_books 
 end
