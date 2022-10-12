66f922e07315c185d339cabd8abf9b79abc269eb
Anthony Kimeu <tkymeu@gmail.com>
2022-10-12 10:28:44 +0300

    milestone:added validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ApplicationRecord
     belongs_to :book_club_book
-    has_many :comments
+    has_many :comments, dependent: :destroy
+
+    validates :bookclub_book_id, {presence: true }
+    validates :question, {presence: true }
+    validates :chapter, {presence: true }
+    validates :chapter, numericality: true
 end
