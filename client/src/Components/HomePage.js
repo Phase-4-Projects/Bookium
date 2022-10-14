2842e71ff9add46f9b7794ed00d876eb671a2880
Anthony Kimeu <tkymeu@gmail.com>
2022-10-14 13:18:57 +0300

    made changes

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
     return (
       <div className="homepage">
         <div id="welcomemessage">
-          <h1 className="welcome-header">Welcome to novel finds</h1>
+          <h1 className="welcome-header">Welcome to Bookium</h1>
           <p>Join a community of bibliophiles. Create or attend book clubs and keep track of what you're reading.</p>
         </div>
       </div>
