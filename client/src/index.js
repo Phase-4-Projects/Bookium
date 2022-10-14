f640b1595d14bd97ae283488c26356e49b623e69
Anthony Kimeu <tkymeu@gmail.com>
2022-10-14 07:09:29 +0300

    updated containers

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './css/index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import store from './Redux/index.js'
 
 ReactDOM.render(
-  <React.Fragment>
-    <App />
-  </React.Fragment>,
-  document.getElementById('root')
-)
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>, document.getElementById('root'));
+
+serviceWorker.unregister();
