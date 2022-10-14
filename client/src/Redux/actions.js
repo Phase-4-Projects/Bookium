dfcbc75476da686898f91e5724662f54924169a8
Anthony Kimeu <tkymeu@gmail.com>
2022-10-14 07:08:35 +0300

    made changes

---
+++ client/src/Redux/actions.js
@@ -0,0 +1,77 @@
+export const handleLogin = (user) => ({type: 'HANDLE_LOGIN', payload: {user}})
+export const logoutUser = () => ({type: 'HANDLE_LOGOUT'})
+export const getCurrentUser = (user) => ({type: 'GET_CURRENT_USER', payload: {user}})
+export const changeCreateEventForm = () => ({type: 'CHANGE_CREATE_EVENT_FORM'})
+export const updateBookObjs = (books) => ({type: 'UPDATE_BOOK_OBJS', payload: books})
+export const updateUserFromFetch = (user) => ({type: 'UPDATE_USER_FROM_FETCH', payload: user})
+export const fetchedBookFromApi = (books) => ({type: 'FETCHED_BOOKS_FROM_API', payload: books})
+export const fetchBookClubs = (clubs) => ({type: 'FETCH_BOOK_CLUBS', payload: clubs})
+export const updateComments = (comments) => ({type: 'UPDATE_COMMENTS', payload: comments})
+export const updateReviews = (reviews) => ({type: 'UPDATE_REVIEWS', payload: reviews})
+export const changeEvent = (club) => ({type: 'CHANGE_EVENT', payload: {club}})
+
+export const loginAndFetch = (e) => {
+  return (dispatch) => {
+    return loginFetch(e)
+    .then(json => dispatch(handleLogin(json.user)) && localStorage.setItem("token", json.jwt))
+  }
+}
+
+export const signUpAndFetch = (e) => {
+  return (dispatch) => {
+    return signUpFetch(e)
+    .then(json => dispatch(handleLogin(json.user)) && localStorage.setItem("token", json.jwt))
+  }
+}
+
+export const setAndFetchUser = (token) => {
+  return (dispatch) => {
+    return fetchingCurrentUser(token)
+    .then(json => dispatch(getCurrentUser(json.user)))
+  }
+}
+
+export const loginFetch = (e) => {
+  return fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        user: {
+          username: e.target.username.value,
+          password: e.target.password.value
+        }
+      })
+    }).then(resp => resp.json())
+}
+
+export const signUpFetch = (e) => {
+  return fetch("/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+        first_name: e.target.firstname.value,
+        last_name: e.target.lastname.value,
+        city: e.target.city.value,
+        state: e.target.state.value,
+        image: e.target.image.value,
+        username: e.target.username.value,
+        password: e.target.password.value
+      }
+      })
+    }).then(resp => resp.json())
+}
+
+export const fetchingCurrentUser = (token) => {
+  return fetch('/profile', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  .then(resp => resp.json())
+}
---
+++ client/src/Redux/reducer.js
@@ -0,0 +1,91 @@
+const initialState = {
+    auth: {},
+    books: {
+      read: [],
+      wantToRead: [],
+      currentlyReading: []
+    },
+    club: {},
+    createEventForm: false,
+    bookObjs: [],
+    fetchedBooks: [],
+    bookClubs: [],
+    comments: [],
+    reviews: []
+  }
+  
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+      case('HANDLE_LOGIN'): {
+        return {...state, auth: action.payload,
+          books: {read: action.payload.user.read,
+          wantToRead: action.payload.user.want_to_read,
+          currently_reading: action.payload.user.currently_reading}
+        }
+      }
+      case('HANDLE_LOGOUT'): {
+        return {...state, auth: {},
+          books: {
+            read: {},
+            wantToRead: {},
+            currentlyReading: {}
+            }
+          }
+        }
+      case('GET_CURRENT_USER'): {
+        return {...state, auth: action.payload,
+           books: {
+             read: action.payload.user.read,
+             wantToRead: action.payload.user.want_to_read,
+             currentlyReading: action.payload.user.currently_reading
+            }
+          }
+        }
+      case('CHANGE_CREATE_EVENT_FORM'): {
+        return { ...state,
+          createEventForm: !state.createEventForm
+        }
+      }
+      case('UPDATE_BOOK_OBJS'): {
+        return { ...state,
+          bookObjs: action.payload
+        }
+      }
+      case('UPDATE_USER_FROM_FETCH'): {
+        return {...state,
+          auth: {
+            user: action.payload
+        }
+      }
+    }
+      case('FETCHED_BOOKS_FROM_API'): {
+        return { ...state,
+          fetchedBooks: action.payload
+        }
+      }
+      case('FETCH_BOOK_CLUBS'): {
+        return { ...state,
+          bookClubs: action.payload
+        }
+      }
+      case('CHANGE_EVENT'): {
+        return {...state,
+          club: action.payload.club
+        }
+      }
+      case('UPDATE_COMMENTS'): {
+        return {...state,
+           comments: action.payload
+         }
+      }
+      case('UPDATE_REVIEWS'): {
+        return {...state,
+           reviews: action.payload
+         }
+      }
+      default:
+        return state
+    }
+  }
+  
+  export default reducer
