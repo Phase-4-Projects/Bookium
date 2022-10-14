024be41a99a346c86d5479771c43a9371c509e5f
Anthony Kimeu <tkymeu@gmail.com>
2022-10-14 07:11:06 +0300

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
-  namespace :api do 
-    patch "/bookclubs/:id/current-book", to: "bookclubs#current_book"
-    resources :book_clubs
-
-    resources :book_club_books, only: [:index, :destroy, :update]
-    
-    resources :goals, only: [:show, :create, :update, :destroy]
-    
-    resources :questions, only: [:show, :create, :update, :destroy]
-    
-    resources :book_club_users
-    
-    resources :users
-  end
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  resources :comments
+  resources :user_books
+  resources :user_events
+  resources :reviews
+  resources :books
+  resources :events
+  resources :users
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
